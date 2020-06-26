aaab450f0ae8847b3583ced17cca6844dd41c0a4
Masayuki Ono <mono0926@gmail.com>
2020-06-26 11:22:59 +0900

    Add initial project

---
+++ example/lib/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'pages/pages.dart';
+import 'router.dart';
+import 'theme.dart';
+
+class App extends StatelessWidget {
+  const App();
+
+  @override
+  Widget build(BuildContext context) {
+    const title = 'mono_kit Demo';
+    return MaterialApp(
+      navigatorKey: context.watch(),
+      theme: buildLightTheme(),
+      darkTheme: buildDarkTheme(),
+      title: title,
+      home: const HomePage(),
+      onGenerateRoute: Provider.of<Router>(context).onGenerateRoute,
+    );
+  }
+}
