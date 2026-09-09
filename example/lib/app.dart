eb868a361efcc9a7d52a92f2d913922002a4ec92
mono — Masayuki Ono <mono0926@gmail.com>
2026-09-09 12:46:17 +0900

    feat: migrate to Dart 3, sound null safety, and flutter_portal 1.x (#4)

--- example/lib/app.dart
+++ example/lib/app.dart
@@ -1,26 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_portal/flutter_portal.dart';
-import 'package:mono_kit/mono_kit.dart';
-import 'package:provider/provider.dart';
 
 import 'pages/pages.dart';
-import 'router.dart';
 
 class App extends StatelessWidget {
-  const App();
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const title = 'mono_kit Demo';
     return MaterialApp(
-      navigatorKey: context.watch(),
-      theme: lightTheme(),
-      darkTheme: darkTheme(),
-      title: title,
-      home: const HomePage(),
-      onGenerateRoute: Provider.of<Router>(context).onGenerateRoute,
-      builder: (context, child) => Portal(
-        child: child,
+      title: 'Cupertino Menu Example',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const Portal(
+        child: HomePage(),
       ),
     );
   }
